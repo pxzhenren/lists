e67f6a17fa7de3cf653aaefd4912c909e02eb7b8
Jill Zhang <jz19911119@gmail.com>
2025-04-15 12:52:33 -0400

    Delete script directory

--- script/script.js
+++
@@ -1,5 +0,0 @@
-function displayText() {
-    const input = document.getElementById('textInput').value;
-    const output = document.getElementById('formattedOutput');
-    output.textContent = input;
-}
