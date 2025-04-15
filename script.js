f2c28623cbcb91b62d0754cc0c3e45b8f858e53e
Jill Zhang <jz19911119@gmail.com>
2025-04-15 12:52:01 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function displayText() {
+    const input = document.getElementById('textInput').value;
+    const output = document.getElementById('formattedOutput');
+    output.textContent = input;
+}
